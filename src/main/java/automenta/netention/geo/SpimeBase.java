32934e1714196732c81f21a4ca90ca5109945b4e
SeH <1s1e1h1@gmail.com>
2015-07-08 23:14:23 -0400

    spime websocket connect

--- src/main/java/automenta/netention/geo/SpimeBase.java
+++ src/main/java/automenta/netention/geo/SpimeBase.java
@@ -5,13 +5,15 @@ import org.apache.lucene.search.Query;
 import org.hibernate.search.query.dsl.EntityContext;
 import org.hibernate.search.query.dsl.QueryBuilder;
 import org.infinispan.Cache;
-import org.infinispan.configuration.cache.Configuration;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.configuration.cache.Index;
+import org.infinispan.manager.CacheContainer;
 import org.infinispan.manager.DefaultCacheManager;
 import org.infinispan.query.CacheQuery;
 import org.infinispan.query.Search;
 import org.infinispan.query.SearchManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Iterator;
 
@@ -21,10 +23,19 @@ import java.util.Iterator;
  */
 public class SpimeBase implements Iterable<NObject> {
 
+    public static final Logger log = LoggerFactory.getLogger(SpimeBase.class);
 
     private final Cache<String, NObject> cache;
     private final SearchManager search;
     private final EntityContext nobjContext;
+
+    public SpimeBase(CacheContainer cm, String id) {
+        this(cm.getCache(id));
+    }
+
+    public SpimeBase(CacheContainer cm) {
+        this(cm.getCache());
+    }
 
     public SpimeBase(Cache<String, NObject> cache) {
 
@@ -41,20 +52,69 @@ public class SpimeBase implements Iterable<NObject> {
         return new NObject(id);
     }
 
+    public static ConfigurationBuilder withIndexing(ConfigurationBuilder cb) {
+        return withIndexing(cb, "ram");
+    }
+
+    public static ConfigurationBuilder withIndexing(ConfigurationBuilder cb, String directoryProvider) {
+        cb.indexing()
+                .index(Index.ALL)
+                .addProperty("default.directory_provider", directoryProvider)
+                .addProperty("lucene_version", "LUCENE_CURRENT");
+        return cb;
+    }
+
+    public static SpimeBase disk(String diskPath, int maxEntries) {
+        ConfigurationBuilder c = new ConfigurationBuilder();
+        c.persistence()
+                .addSingleFileStore()
+                .location(diskPath)
+                .maxEntries(maxEntries)
+                .fetchPersistentState(true)
+                .ignoreModifications(false)
+
+                        //.purgeOnStartup(true)
+
+                .unsafe();
+                /*.clustering()
+                        //.cacheMode(CacheMode.DIST_SYNC)
+                .cacheMode(CacheMode.DIST_SYNC)
+                .sync()
+                .l1().lifespan(25000L)
+                .hash().numOwners(3);*/
+
+        withIndexing(c);
+
+        return newSpimeBase(c);
+    }
+
     /** in-RAM index */
     public static SpimeBase memory() {
-        Configuration infinispanConfiguration = new ConfigurationBuilder()
-                .indexing()
-                .index(Index.ALL)
-                .addProperty("default.directory_provider", "ram")
-                .addProperty("lucene_version", "LUCENE_CURRENT")
-                .build();
 
-        DefaultCacheManager cacheManager = new DefaultCacheManager(infinispanConfiguration);
+        ConfigurationBuilder c = new ConfigurationBuilder();
+        withIndexing(c);
+
+
+        return newSpimeBase(c);
+    }
+
+    public static SpimeBase newSpimeBase(ConfigurationBuilder c) {
+
+        final DefaultCacheManager cacheManager = new DefaultCacheManager(c.build());
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            log.info("Shutting down...");
+            cacheManager.stop();
+        }));
 
         SpimeBase db = new SpimeBase(cacheManager.getCache());
+
+        log.info("Created: " + db + " with " + c);
+
+
         return db;
     }
+
 
     public static void main(String[] args) {
 
@@ -95,4 +155,14 @@ public class SpimeBase implements Iterable<NObject> {
     public Iterator<NObject> iterator() {
         return cache.values().iterator();
     }
+
+    public boolean isEmpty() {
+        return cache.isEmpty();
+    }
+
+    public int size() {
+        return cache.size();
+    }
+
+
 }
---
+++ src/main/java/automenta/netention/run/SpimeServer.java
@@ -0,0 +1,67 @@
+package automenta.netention.run;
+
+
+import automenta.netention.geo.ImportKML;
+import automenta.netention.geo.SpimeBase;
+import automenta.netention.web.ClientResources;
+import automenta.netention.web.Web;
+import automenta.netention.web.WebSocketCore;
+import com.fasterxml.jackson.databind.JsonNode;
+import io.undertow.websockets.core.WebSocketChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class SpimeServer extends Web {
+
+    public class SpimeSocket extends WebSocketCore {
+
+        public SpimeSocket() {
+            super(true);
+        }
+
+        @Override
+        protected void onJSONMessage(WebSocketChannel socket, JsonNode j) {
+            super.onJSONMessage(socket, j);
+        }
+    }
+
+    public static final Logger log = LoggerFactory.getLogger(SpimeServer.class);
+
+    private final SpimeBase base;
+
+    public SpimeServer(SpimeBase s)  {
+        super();
+
+        //static content
+        add("/", ClientResources.handleClientResources());
+
+        //websocket
+        add("/ws", new SpimeSocket().get());
+
+        this.base = s;
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        SpimeBase es = SpimeBase.disk("/tmp/sb", 128*1024);
+
+
+        if (es.isEmpty()) {
+            System.out.println("Initializing database...");
+
+            new ImportKML(es).url("main",
+                    "file:///tmp/kml/EOL-Field-Projects-CV3D.kmz"
+                    //"file:///tmp/kml/GVPWorldVolcanoes-List.kmz"
+            ).run();
+
+        }
+
+        System.out.println(es.size() + " objects loaded");
+
+
+        new SpimeServer(es).start("localhost", 8080);
+    }
+}
