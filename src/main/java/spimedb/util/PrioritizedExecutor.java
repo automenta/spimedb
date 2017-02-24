1e5d71045458bff3c8cddd803ebb4be65e76ee15
SeH <seh1seh@yandex.com>
2017-02-24 13:49:42 -0500

    threadpool updates

--- src/main/java/spimedb/util/PrioritizedExecutor.java
+++ src/main/java/spimedb/util/PrioritizedExecutor.java
@@ -1,30 +1,45 @@
 package spimedb.util;
 
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Comparator;
-import java.util.concurrent.Executor;
-import java.util.concurrent.PriorityBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.*;
 
 /**
  * Created by me on 2/3/17.
  */
 public class PrioritizedExecutor implements Executor {
 
-    private static final float DEFAULT_PRIORITY = 0.5f;
+    final static Logger logger = LoggerFactory.getLogger(PrioritizedExecutor.class);
 
-    public final PriorityBlockingQueue<Runnable> pq = new PriorityBlockingQueue<Runnable>(128, new ComparePriority());
+    private static final float DEFAULT_PRIORITY = 0.5f;
+    private static final long DEFAULT_TIMEOUT_ms = 30 * 1000; //30 SECONDS
+
+    public final PriorityBlockingQueue pq = new PriorityBlockingQueue<>(
+            64 * 1024,
+            runCompare);
 
     private final Executor exe;
 
     public PrioritizedExecutor(int threads) {
-         this.exe = new ThreadPoolExecutor(threads, threads, 1, TimeUnit.SECONDS, pq);
+        //similar to Fixed-Size threadpool
+        this.exe = new ThreadPoolExecutor(threads, threads, 0, TimeUnit.MILLISECONDS, pq);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(this::onShutDown));
+    }
+
+    protected void onShutDown() {
+        pq.forEach(x -> {
+            logger.info("not executed: {}", x);
+        });
     }
 
     public void run(float pri, Runnable r) {
-        run(new MyRunWithPriority(pri, r));
+        run(new MyRunWithPriority(pri, r, DEFAULT_TIMEOUT_ms));
     }
 
     public void run(RunWithPriority r) {
@@ -38,13 +53,13 @@ public class PrioritizedExecutor implements Executor {
 
 
     public interface RunWithPriority extends Runnable {
-        public float pri();
+        float pri();
     }
 
-    private static class ComparePriority<T extends RunWithPriority> implements Comparator<T> {
+    static final Comparator<RunWithPriority> runCompare = new Comparator<RunWithPriority>() {
 
         @Override
-        public int compare(T x, T y) {
+        public int compare(RunWithPriority x, RunWithPriority y) {
             if (x == y) return 0;
 
             int c = Float.compare(y.pri(), x.pri());
@@ -53,14 +68,18 @@ public class PrioritizedExecutor implements Executor {
             }
             return c;
         }
-    }
+    };
 
-    private static class MyRunWithPriority implements RunWithPriority {
+    final Timer timer = new Timer(true);
+
+    class MyRunWithPriority implements RunWithPriority {
         private final float pri;
         private final Runnable r;
+        private final long timeoutMS;
 
-        public MyRunWithPriority(float pri, Runnable r) {
+        public MyRunWithPriority(float pri, Runnable r, long timeoutMS) {
             this.pri = pri;
+            this.timeoutMS = timeoutMS;
             this.r = r;
         }
 
@@ -71,7 +90,53 @@ public class PrioritizedExecutor implements Executor {
 
         @Override
         public void run() {
-            r.run();
+
+            TimeOutTask timeout = new TimeOutTask(Thread.currentThread());
+            timer.schedule(timeout, timeoutMS);
+
+            try {
+                r.run();
+            } catch (ThreadDeath ie) {
+                logger.error("{} interrupted {}", r, ie.getMessage());
+            }
+
+            timeout.cancel();
+
         }
     }
+
+
+    static class TimeOutTask extends TimerTask {
+        final Thread t;
+
+        TimeOutTask(Thread t) {
+            this.t = t;
+        }
+
+        public void run() {
+            if (t != null && t.isAlive()) {
+                t.stop();
+            }
+        }
+    }
+
+
+//    public static void main(String args[]) {
+//        new PoolTest();
+//    }
+
+//        public PoolTest()
+//        {
+//            try
+//            {
+//                PooledExecutor pe = new PooledExecutor(3);
+//                pe.execute(new MyRunnable());
+//                pe.shutdownAfterProcessingCurrentlyQueuedTasks();
+//            }
+//            catch (Exception e)
+//            {
+//                e.printStackTrace();
+//            }
+//        }
+
 }
