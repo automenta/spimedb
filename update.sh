99bc37d49de92628b8f88d47df169548e01c063b
Jim Northrup <jim@vsiwest.com>
2017-04-08 16:19:39 +0700

    CLEAN=t ./update

--- update.sh
+++ update.sh
@@ -15,7 +15,9 @@ echo 'Updating from git'
 git pull
 
 echo 'Update dependency: JCog'
-git clone --depth 1 https://github.com/automenta/narchy/ 2> /dev/null # just to be sure
+[[ $CLEAN ]] && rm -fr ~/.m2/repository/automenta  ~/.m2/repository/opennars  narchy
+
+git clone --depth 1 https://github.com/automenta/narchy 2> /dev/null # just to be sure
 pushd .
     cd narchy
     git pull||exit 1
@@ -23,12 +25,16 @@ popd
 
 echo 'Build dependency: JCog'
 pushd .
+
+
 	cd narchy/util
 	mvn install -DskipTests=true||exit 1
 popd
 
 
 echo 'Build SpimeDB'
+
+[[ $CLEAN ]] && mvn clean
 mvn install -DskipTests=true||exit 1
 
 
